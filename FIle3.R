65616974275786942c969785a9de068a3b985ee5
MaheshDhompare <mdhompare@gmail.com>
2020-02-21 13:58:25 +0530

    Day2

--- FIle3.R
+++ FIle3.R
@@ -102,4 +102,109 @@ str(students)
 students %>% group_by(gender) %>% tally()
 
 students
-students 
+students
+
+
+#Day2:21-Feb-2020----
+library(dplyr)
+mtcars
+table(mtcars$cyl)
+mtcars %>% group_by(cyl) %>% tally()
+mtcars %>% group_by(cyl) %>% summerise(COUNT=n())
+ftable(mtcars$cyl)
+
+#gear and cyl
+mtcars %>% group_by(gear,cyl) %>% tally()
+table(mtcars$cyl, mtcars$gear, dnn=c('cylinder', 'gear'))
+
+df=mtcars #asigning df as mtcars
+head(df) #first 6 records
+tail(df)
+df$am= ifelse(df$am==0, 'Auto', 'Manual')
+head(df)
+df$mpg
+df %>% group_by(mpg) %>% tally()
+df=df %>% mutate(TXTYPE = ifelse(am==0, 'Auto', 'Manual'))
+head(df)
+
+#increase mileage by 10%
+df$mpg * 1.1
+head(df)
+#add mpg+wt into new column MGPWT
+df$mpg + df$wt
+df$MPGWT=df$mpg*1.1 + df$wt
+head(df)
+
+#top 2 cars from mpg from each gear type : use group_by & top_n
+?top_n
+df
+df %>% group_by(gear) %>% top_n(2, wt=mpg) %>% select(gear,mpg)
+
+#lowest 2 cars from mpg from each gear type : use group_by & top_n
+df %>% group_by(gear) %>% top_n(2, wt=-mpg) %>% select(gear,mpg)
+
+#list out details of any 2 cars picked randomly: then do 25% of the cars
+sample_n(df, 2, T)
+sample_frac(df, 0.25, T) %>%  arrange(gear, desc(mpg))
+df %>% sample_frac(0.25) %>%  arrange(gear, desc(mpg))
+
+
+#find mean weight, mileage, and HP for different gears
+df %>% group_by(gear) %>% summarise(avg=mean(mpg))
+df %>% group_by(gear) %>% summarise_at(c('mpg', 'wt', 'hp', 'disp'), c(mean))
+df %>% select(gear, mpg, wt, hp, disp) %>% group_by(gear) %>% summarise_all(mean)
+
+
+
+#graphs----
+hist((df$mpg), col=(1:5))
+barplot(table(df$gear), col=(1:5))
+pie(table(df$gear))
+plot(df$wt, df$mpg)
+
+L1=paste(round(table(df$gear)/nrow(df)*100), '%') #pie chart with % and color
+pie(table(df$gear), labels=L1, col=1:4)
+
+
+#install ggplot2 and reshape2
+library(ggplot2)
+library(reshape2)
+
+(rollno = paste('IIM', 1:10, sep='_'))
+(name = paste ('SName', 1:10, sep=''))
+(gender = sample(c('M', 'F'), size=10, replace=T))
+(program = sample(c('BBA','MBA'), size=10, replace = T))
+(marketing = trunc(rnorm(10, mean=60, sd=10)))
+(finance = trunc(rnorm(10, mean=55, sd=12)))
+(Operations = trunc(rnorm(10, mean=70, sd=5)))
+students <- data.frame(rollno, name, gender, program, marketing, finance, stringsAsFactors = F)
+students
+head(students)
+
+(meltSum1 = melt(students, id.vars = c('rollno', 'name', 'gender', 'program'), variable.name = 'subject', value.name = 'marks'))
+meltSum1
+head(meltSum1)
+meltSum1 %>% group_by(program, gender, subject) %>% summarise(MeanMarks = mean(marks))
+sum2 = meltSum1 %>% group_by(program, gender, subject) %>% summarise(MeanMarks = mean(marks))
+head(sum2)
+#ggplot(data=sum2, aes(x=gender, y=MeansMarks,fill=gender)) + geom_bar(stat='identity') + facet_grid()
+students
+head(students, n=2)
+students %>% group_by(gender) %>% summarise(COUNT = n())
+ggplot(students %>% group_by(gender) %>% summarise(COUNT = n()), aes(x=gender, y=COUNT, fill=gender)) + geom_bar(stat='identity')
+ggplot(students,aes(x=gender, y=..count..))+geom_bar(stat='count')
+
+#stakedbar
+ggplot(students %>% group_by(gender, program) %>% summarise(COUNT = n()), aes(x=gender, y=COUNT, fill=program)) + geom_bar(stat='identity') + geom_text(aes(label=COUNT))+labs(title = 'gender wise count')
+
+
+#side by side
+ggplot(students %>% group_by(gender, program) %>% summarise(COUNT = n()), aes(x=gender, y=COUNT, fill=program)) + geom_bar(stat='identity', position = position_dodge2(.7)) + geom_text(aes(label=COUNT))+labs(title = 'gender wise count')
+
+
+#subject -program - gender - mean marks
+names(students)
+names(meltSum1)
+head(meltSum1)
+ggplot(meltSum1 %>% group_by(gender, program, subject) %>% summarise(meanMarks = round(mean(marks))), aes(x=gender, y=meanMarks, fill=program)) + geom_bar(stat='identity', position = position_dodge2(.7)) + labs(title = 'subject -program - gender - mean marks') + facet_grid(~subject) + geom_text(aes(label=meanMarks))
+                                                                                                                                                                                                                                                                                                        
